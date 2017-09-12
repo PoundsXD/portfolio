3ef194658fa7e96e967945facad62d4d9aa9392e
Brian Wheeler <monsteraono@gmail.com>
2017-09-12 10:31:24 -0700

    Made initial html framework and started working on project constructor for page.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var projectArray = [];
+function ProjectData(name, date, sitePath) {
+  this.name = name;
+  this.date = date;
+  this.sitePath = sitePath;
+  this.push(projectArray);
+}
+var aboutMe = new ProjectData('About Me', 'Code Fellows 201 Week 1', 'url');
+var salmonCookie = new ProjectData('Salmon Cookies', 'Code Fellows 201 Week 2', 'url');
+var busMall = new ProjectData('Bus Mall', 'Code Fellows 201 Week 3', 'url');
+var homeWorkout = new ProjectData('HomeFit', 'Code Fellows 201 Project Week', 'url');
