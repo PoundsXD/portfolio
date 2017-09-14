996a552d34a235113a66fd2a085e6636a441a7ee
Brian Wheeler <monsteraono@gmail.com>
2017-09-14 12:00:53 -0700

    Made some style changes to the about me div and how far below the nav it would sit.

--- js/app.js
+++ js/app.js
@@ -13,22 +13,22 @@ var busMall = new ProjectData('Bus Mall', 'Code Fellows 201 Week 3', 'url');
 var homeWorkout = new ProjectData('HomeFit', 'Code Fellows 201 Project Week', 'url');
 
 $(document).ready(function() {
-  var dynamicNav = $('nav').outerHeight(true);
+  var dynamicNav = $('nav').outerHeight(true) + 15;
   $('.aboutMe').css('margin-top', dynamicNav);
 });
 $(window).resize(function() {
-  var dynamicNav = $('nav').outerHeight(true);
+  var dynamicNav = $('nav').outerHeight(true) + 15;
   $('.aboutMe').css('margin-top', dynamicNav);
 });
 $('.navDropdownEd').on('click', function() {
   $('.navDropdownEd').addClass('extended');
   $('.education').css('display', 'block');
-  var dynamicNav = $('nav').outerHeight(true);
+  var dynamicNav = $('nav').outerHeight(true) + 15;
   $('.aboutMe').css('margin-top', dynamicNav);
 });
 $('.education').on('click', function() {
   $('.navDropdownEd').removeClass('extended');
   $('.education').css('display', 'none');
-  var dynamicNav = $('nav').outerHeight(true);
+  var dynamicNav = $('nav').outerHeight(true) + 15;
   $('.aboutMe').css('margin-top', dynamicNav);
 });
